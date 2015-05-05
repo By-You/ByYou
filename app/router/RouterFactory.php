2c2a41ce6ad3eba2ff1c747918078b195c100611
B4rtosek <B4rtosek@seznam.cz>
2015-05-05 21:34:46 +0200

    Úprava route - rozeznává Front / Admin module

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -21,11 +21,18 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList();
-        $router[] = new Route('<presenter>/<action>', array(
+        /*$router[] = new Route('admin/<module (?!Front)[^/]+>/<presenter>/<action>', array(
             'module' => 'Front',
             'presenter' => 'Homepage', 'Product', 'About', 'Kontakt',
-            'action' => 'default',
-        ));
+            'action' => 'default',*/
+        $member = new RouteList('Admin');
+        $member[] = new Route('admin/<presenter>/<action>', 'Homepage:default');
+        $router[] = $member;
+
+        $public = new RouteList('Front');
+        $public[] = new Route('<presenter>/<action>', 'Homepage:default');
+        $router[] = $public;
+
 		return $router;
 	}
 
