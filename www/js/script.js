90f55ee5ff127c801c9f35c026b6c2f64e925745
B4rtosek <B4rtosek@seznam.cz>
2015-05-06 15:46:53 +0200

    Nahrání Witti šablony do modulu Admin

---
+++ www/js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+//slider
+  $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+//navigace
+  $('.menu_nav ul li a').css({"border-top-left-radius":"6px", "border-top-right-radius":"6px", "-moz-border-radius-topleft":"6px", "-moz-border-radius-topright":"6px", "-webkit-border-top-left-radius":"6px", "-webkit-border-top-right-radius":"6px"});
+});	
+
+Cufon.replace('h1, h2, h3, h4, h5, h6, .content .mainbar a.com, .content .mainbar a.rm', { hover: true });
